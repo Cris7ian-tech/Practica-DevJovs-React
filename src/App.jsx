4efd0a0415054788bda139e09502835355f585d9
unknown <Cristianlucero.ala2@gmail.com>
2025-12-10 06:35:03 -0300

    paginacion fincionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import Header from './components/Header'
 import FiltroCentarl from './components/FiltroCentarl'
 import Paginacion from './components/Paginacion'
@@ -5,13 +7,20 @@ import Footer from './components/Footer'
 
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1)
+  const totalPages = 5
+
+  const handlePageChange = (page) => {
+    console.log(`Navegando a la página ${page}`);
+    setCurrentPage(page);
+  }
   
   return (
     <>
     <Header />
     <main>
       <FiltroCentarl />
-      <Paginacion />
+      <Paginacion currentPage={currentPage} totalPages={5} onPageChange={handlePageChange}/>
     </main>
     <Footer />
     </>
--- src/components/Paginacion.jsx
+++ src/components/Paginacion.jsx
@@ -1,14 +1,37 @@
-import JobCard from "./JobCard"
+import JobCard from "./JobCard" 
 
-const Paginacion = ({currentPage = 1, totalPages = 10}) => {
+
+
+const Paginacion = ({currentPage = 1, totalPages = 10, onPageChange}) => {
+  console.log("Render Paginacion")
   // Generar arrary de paginas a mostrar
   const pages = Array.from ({length: totalPages}, (_, index)=> index + 1)
 
-  const isFristPage = currentPage === 1
+  const isFirstPage = currentPage === 1
   const isLastPage = currentPage === totalPages
 
-  const stylePrevButton = isFristPage ? { PointerEvents: "none", opacity: 0.5 } : {}
-  const styleNextButton = isLastPage ? { PointerEvents: "none", opacity: 0.5 } : {}
+  const stylePrevButton = isFirstPage ? {pointerEvent: "none", opacity: 0.5} : {}
+  const styleNextButton = isLastPage ? {pointerEvent: "none", opacity: 0.5} : {}
+
+  const handlePrevClick = (event) => {
+    event.preventDefault()
+    if (isFirstPage === false) {
+      onPageChange(currentPage - 1)
+    }
+  }
+
+  const handleNextClick = (event) => {
+    event.preventDefault()
+    if (isLastPage === false) {
+      onPageChange(currentPage + 1)
+    }
+  }
+
+  const handleChangePage = (event, page) => {
+    event.preventDefault()
+    if (page !== currentPage)
+    onPageChange(page)
+  }
 
   return (
     <>
@@ -21,29 +44,34 @@ const Paginacion = ({currentPage = 1, totalPages = 10}) => {
         </div>
 
         <nav className="pagination">
-          <a href="#" style={stylePrevButton}>
-            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
-              strokeLinecap="round" strokeLinejoin="round">
+          
+          <a href="#" style={stylePrevButton} onClick={handlePrevClick}>
+            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5"
+              strokeLinecap="round" strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left">
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M15 6l-6 6l6 6" />
-            </svg></a>
+            </svg>
+          </a>            
           
           {pages.map(page => (
             <a
               href="#"
-              className={currentPage === page ? "active" : ""}
+              className={currentPage === page ? "is-active" : ""}
+              onClick={(event) =>handleChangePage(event, page)}
             >
               {page}
             </a>
           ))}
-
-          <a href="#" style={styleNextButton}>
+            
+          <a href="#" style={styleNextButton} onClick={handleNextClick}>
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5"
               strokeLinecap="round" strokeLinejoin="round"
               className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right">
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M9 6l6 6l-6 6" />
-            </svg></a>
+            </svg>
+          </a>
         </nav>
       </section>
     </>
